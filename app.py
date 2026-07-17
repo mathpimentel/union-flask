4f213f7409f1920a3beb6bcc530693bd66a20d54
Matheus Pimentel <mathcamposp@gmail.com>
2026-07-17 12:47:04 -0300

    refactor: load task data from data module

--- app.py
+++ app.py
@@ -1,40 +1,10 @@
 from flask import Flask, request, jsonify
 from models.task import Task
-# __name__ = "__main__"
+from data import tasks as task_list
+
 app = Flask(__name__)
 
-tasks = [
-    Task(
-        id=1,
-        title="Estudar Flask",
-        description="Aprender os conceitos básicos do Flask.",
-        completed=False
-    ),
-    Task(
-        id=2,
-        title="Implementar API REST",
-        description="Criar os endpoints GET, POST, PUT e DELETE.",
-        completed=True
-    ),
-    Task(
-        id=3,
-        title="Documentar API",
-        description="Gerar documentação utilizando Swagger/OpenAPI.",
-        completed=False
-    ),
-    Task(
-        id=4,
-        title="Escrever testes",
-        description="Criar testes unitários para os endpoints da API.",
-        completed=False
-    ),
-    Task(
-        id=5,
-        title="Publicar projeto",
-        description="Realizar o deploy da aplicação em um servidor.",
-        completed=True
-    )
-]
+tasks = task_list.tasks
 task_id_control = len(tasks) 
 
 
@@ -67,7 +37,7 @@ def create_task ():
     )
 
     tasks.append(new_task)
-    return jsonify({"message": f"A tarefa '{new_task.title}' foi adicionada com sucesso."})
+    return jsonify({"message": f"A tarefa '{new_task.title}' foi adicionada com sucesso.", "id": new_task.id})
 
 @app.route("/tasks/<int:id>", methods = ["PUT"])
 def update_task(id):
